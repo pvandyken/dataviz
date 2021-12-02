7cdd59d723d34b3028ae1c4a9b5336b9ebdfe7e7
Tristan Kuehn <tristankuehn@gmail.com>
2021-12-02 11:21:26 -0500

    Initial category selector

--- components/category-selector.tsx
+++ components/category-selector.tsx
@@ -1,3 +1,59 @@
-export const CategorySelector = () => {
-    
+import { Category } from "../interfaces/dimensions"
+import React from "react"
+
+interface CategorySelectorProps {
+    categories: Category[];
 }
+
+interface CategoryOptionProps {
+    category: Category;
+    parent: CategorySelector;
+}
+
+class CategoryOption extends React.Component {
+    constructor(props: CategoryOptionProps) {
+        super(props);
+        this.state = {active: false};
+    }
+
+    render() {
+        return (
+            <li className="nav-item">
+                <a className={"nav-link " + (this.state.active ? "active" : "")} href="#">
+                    {this.props.category.name}
+                </a>
+            </li>
+        );
+    }
+}
+
+class CategorySelector extends React.Component {
+    constructor(props: CategorySelectorProps) {
+        super(props);
+        this.state = {activeCategory: null};
+    }
+
+    changeActive(categoryOption: CategoryOption) {
+        if (this.state.activeCategory) {
+            this.state.activeCategory.state.active = false;
+        }
+        categoryOption.state.active = true;
+        this.state.activeCategory = categoryOption;
+    }
+
+    render() {
+        const categoryItems = this.props.categories.map((category) =>
+            <CategoryOption category={category} parent={this} />
+        );
+        return (
+            <nav>
+                <ul className="navbar-nav flex-column">
+                    <li><span className="navbar-brand">Categories</span></li>
+                    {categoryItems}
+                </ul>
+            </nav>
+        );
+    }
+}
+
+export { CategorySelector }
