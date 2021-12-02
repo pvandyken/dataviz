eb8e577229d153fa1aef492a71c2b0202591d0aa
Peter Van Dyken <pcvandyken@gmail.com>
2021-12-02 17:11:37 -0500

    Update dimension interfaces to specify type and use Slice

--- interfaces/dimensions.ts
+++ interfaces/dimensions.ts
@@ -1,12 +1,15 @@
+import { Slice } from "../lib/slice";
+
 export type DimensionTypes = string | number;
 
 export interface Dimension<T extends DimensionTypes> {
     name: string;
     possibleValues: T[];
+    type: string;
 }
 
 export interface ActiveDimension<T extends DimensionTypes> extends Dimension<T> {
-    value: T 
+    value: Slice<T>
 }
 
 export interface Category {
