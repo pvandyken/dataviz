5fc22e2b150c70acb2b80d672230871057364214
Peter Van Dyken <pcvandyken@gmail.com>
2021-12-02 15:06:53 -0500

    Dimension manager handles activeDimension state

--- components/dimension-manager.tsx
+++ components/dimension-manager.tsx
@@ -1,3 +1,13 @@
+import { useState } from "react"
+import { ActiveDimension, DimensionTypes } from "../interfaces/dimensions"
+
+type ActiveDimensions = ActiveDimension<DimensionTypes>[] | undefined;
+
 export const DimensionManager = () => {
-    
+    const [activeDimensions, setActiveDimensions] = useState<ActiveDimensions>(undefined);
+
+    return (
+        <div></div>
+    )
+
 }
