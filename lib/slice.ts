c20e3ac5849206ac79a28108c30df79e45a79518
Peter Van Dyken <pcvandyken@gmail.com>
2021-12-02 17:09:32 -0500

    Expanded api and add utility function

--- lib/slice.ts
+++ lib/slice.ts
@@ -1,19 +1,36 @@
-import { DimensionTypes } from "../interfaces/dimensions";
+import { DimensionTypes, Dimension, ActiveDimension } from "../interfaces/dimensions";
 
 export class Slice<T extends DimensionTypes> {
     /**
-    * Should receive values or ranges of values in the case of integers
-    * For instance: 
-    *   ["right"]
-    *   ["right", "left"]
-    *   1-4, 5-20, 31-100
-    *   all
-    * 
     * This will be used to define values within Dimension. It will be a child of
     * Dimension, so Dimension should not be an argument in the constructor
     */
 
-    constructor() {
+    constructor(values?: T) {
+        /**
+         * If no value passed to constructor, assume the slice covers all possible values
+         */
 
     }
+
+    contains(value: T) {
+        /**
+         * Test if slice contains value
+         */
+        return true;
+    }
+
+    isSingle() {
+        /**
+         * Return true if slice is a single value: aka not "all"
+         */
+    }
+}
+
+export const getSlicedDimensions = (dimensions: ActiveDimension<DimensionTypes>[]) => {
+    return dimensions.filter(dimension => {
+        return dimension.value.isSingle()
+    }).map(dimension => {
+        return dimension.name
+    })
 }
