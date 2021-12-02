9e7f45925ddb7d20e9329712029d3f2754dd611b
Peter Van Dyken <pcvandyken@gmail.com>
2021-12-02 17:12:42 -0500

    Start on datapage

--- components/data-page.tsx
+++ components/data-page.tsx
@@ -1,6 +1,56 @@
-import { DimensionTypes } from "../interfaces/dimensions"
+import { ActiveDimension, DimensionTypes } from "../interfaces/dimensions"
+import Image from "next/image"
+import { getSlicedDimensions } from "../lib/slice"
 
-export const DataPage = () => {
+interface Data<T extends object> {
+    type: string;
+    dimensions: {
+        [key: string]: string | null;
+    }
+    properties: T
+}
+
+interface Img {
+    path: string;
+    height: number;
+    width: number;
+}
+
+
+interface DataPageProps {
+    activeDimensions: ActiveDimension<DimensionTypes>[]
+    data: Data<any>[];
+}
+
+
+export const DataPage = ({activeDimensions, data}: DataPageProps) => {
+    const activeItems = data.filter(item => {
+        for (let dim of activeDimensions) {
+            const itemDimValue = item.dimensions[dim.name]
+            if (!itemDimValue) {
+                return false;
+            } else if (dim.value.contains(itemDimValue)) {
+                return true;
+            }
+        }
+        return false;
+    })
+
+    const slicedDimensions = getSlicedDimensions(activeDimensions);
+    
+    const markup = activeItems.map(item => {
+        if (item.type !== "img") {
+            return null;
+        }
+        const labels = slicedDimensions.map(dimension => {
+            return item.dimensions[dimension]
+        })
+        return (
+            <Image src={(item as Data<Img>).properties.path}/>
+        )
+
+    })
+
     
 }
 
