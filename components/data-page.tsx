c8fb5f1735d7f2b1ee140957d52f404e990a85e7
Peter Van Dyken <pcvandyken@gmail.com>
2021-12-03 13:37:57 -0500

    Working prototype of data-page

--- components/data-page.tsx
+++ components/data-page.tsx
@@ -1,45 +1,25 @@
 import { ActiveDimension, DimensionTypes } from "../interfaces/dimensions"
 import Image from "next/image"
 import { getSlicedDimensions } from "../lib/slice"
+import { Data, Img } from "../interfaces/data"
 
-interface Data<T extends object> {
-    type: string;
-    dimensions: {
-        [key: string]: string | null;
-    }
-    properties: T
-}
 
-interface Img {
-    path: string;
-    height: number;
-    width: number;
-}
 
 
 interface DataPageProps {
     activeDimensions: ActiveDimension<DimensionTypes>[]
-    data: Data<any>[];
+    activeItems: Data<any>[];
 }
 
 
-export const DataPage = ({activeDimensions, data}: DataPageProps) => {
-    const activeItems = data.filter(item => {
-        for (let dim of activeDimensions) {
-            const itemDimValue = item.dimensions[dim.name]
-            if (!itemDimValue) {
-                return false;
-            } else if (dim.value.contains(itemDimValue)) {
-                return true;
-            }
-        }
-        return false;
-    })
+export const DataPage = (props: DataPageProps) => {
 
-    const slicedDimensions = getSlicedDimensions(activeDimensions);
+
+    const slicedDimensions = getSlicedDimensions(props.activeDimensions);
+
     
-    const markup = activeItems.map((item, i) => {
-        if (item.type !== "img") {
+    const markup = props.activeItems.map((item, i) => {
+        if (item.type !== "image") {
             return null;
         }
         const labels = slicedDimensions.map((dimension, i) => {
