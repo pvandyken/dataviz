c916090604286e038d601c24785dac1ffc353294
kulasbart <kulasb6@gmail.com>
2021-12-02 22:22:06 -0500

    fixed da type errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,24 @@
 import type { NextPage } from 'next'
 import { DimensionManager } from '../components/dimension-manager'
+import { Category, Dimension } from "../interfaces/dimensions"
 
+
+const dimension1: Dimension<number> = {name: "dimension1", possibleValues: [1, 2], type: "idk"} 
+const dimension2: Dimension<number> = {name: "dimension2", possibleValues: [3, 4], type: "idk"}
+const dimension3: Dimension<number> = {name: "dimension3", possibleValues: [5, 6], type: "idk"}
+const category1 : Category = {
+    id: "test",
+    name: "test",
+    dimensions: [dimension1]
+}
+const category2 : Category = {
+    id: "test2",
+    name: "test2",
+    dimensions: [dimension2, dimension3]
+}
 const exampleProps = [
-    {
-        id: "test",
-        name: "test",
-        dimensions: [{name: "dimension1", possibleValues: [1, 2]}]
-    },
-    {
-        id: "test2",
-        name: "test2",
-        dimensions: [{name: "dimension2", possibleValues: [3, 4]},{name: "dimension3", possibleValues: [5, 6]}]
-    }
+    category1,
+    category2
 ]
 
 const Home: NextPage = () => {
